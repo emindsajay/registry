c1987f73200ea84f9f4cf9115a88e64f9904ca37
shreyvishal <shreya.vishal@eminds.ai>
2025-03-20 21:02:44 +0530

    Plugins.ts for registry

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  registry: {
+    enabled: true,
+    resolve: "./src/plugins/plugins/registry" // path to the plugin folder,
+  },
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "24h"
+      }
+    }
+  }
+});
