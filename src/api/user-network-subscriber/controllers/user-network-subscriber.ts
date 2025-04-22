c633a5505b1c0809e1458913c669d7edb8554168
Ajay Nishad <ajay.nishad@eminds.ai>
2025-04-22 18:10:11 +0530

    feat: remove the collection

--- src/api/user-network-subscriber/controllers/user-network-subscriber.ts
+++
@@ -1,7 +0,0 @@
-/**
- * user-network-subscriber controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::user-network-subscriber.user-network-subscriber');
--- src/api/user-network-subscriber/routes/user-network-subscriber.ts
+++
@@ -1,7 +0,0 @@
-/**
- * user-network-subscriber router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::user-network-subscriber.user-network-subscriber');
