8914f282fb36659bbe8cc5f39fbc6cadc0f2e3f9
Ajay Nishad <ajay.nishad@eminds.ai>
2025-06-02 19:54:54 +0530

    feat: add 20 sec wait in add registry call

--- src/api/dedi/services/dedi.ts
+++ src/api/dedi/services/dedi.ts
@@ -15,6 +15,7 @@ interface RecordData {
 
 export default {
     async addRegistry(namespace: string, data: RegistryData): Promise<any> {
+        await new Promise(resolve => setTimeout(resolve, 20000));
         return await this._makeRequest(`/dedi/${namespace}/add-registry`, "POST", data);
     },
 
